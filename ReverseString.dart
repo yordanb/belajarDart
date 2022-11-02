9ca46cb6894e7bc1d97a915541bae3f5c33aa10e
yordanb <masyordan@gmail.com>
2022-11-03 04:41:25 +0800

    edit balik string

--- ReverseString.dart
+++ ReverseString.dart
@@ -1,9 +1,13 @@
 main() {
   String a = balikString("Yordan");
   print(a);
+  String b = solution("Baniara");
+  print(b);
 }
 
 String balikString(String input) {
   var chars = input.split('');
   return chars.reversed.join();
 }
+
+String solution(str) => str.split('').reversed.join('');
