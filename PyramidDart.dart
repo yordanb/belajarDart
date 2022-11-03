808506057f81b1a4867cc1376fb4a842d592e0f0
yordanb <masyordan@gmail.com>
2022-11-03 21:18:30 +0800

    pyramid by dart

---
+++ PyramidDart.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+main() {
+  int n = 8;
+  print('');
+  for (var i = 1; i <= n; i++) {
+    for (var j = 0; j < i; j++) {
+      stdout.write(' *');
+    }
+    print('');
+  }
+}
